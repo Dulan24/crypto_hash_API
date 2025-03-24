60145c5c090a68ae38d66b99f45e3548d4ec0ad4
Dulan24 <Dulanlokugeegana@gmail.com>
2025-03-24 17:52:11 +0530

    update deployment

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 DataBase_NAME="cyber_key_db"
 DataBase_USER="test"
 DataBase_PASSWORD="BQafKTbGdg2BdnKopwtTPPHO1VGceNEU"
-DataBase_HOST="postgresql://test:BQafKTbGdg2BdnKopwtTPPHO1VGceNEU@dpg-cvgk1j7noe9s73cdtrog-a.singapore-postgres.render.com/cyber_key_db"
+DataBase_HOST="dpg-cvgk1j7noe9s73cdtrog-a.singapore-postgres.render.com"
 DataBase_PORT="5432"
